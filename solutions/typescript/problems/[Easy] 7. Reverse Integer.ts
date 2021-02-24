924a462065694775ff9f65a44e7d831cf20537c0
RageBill <chubillkelvin@gmail.com>
2021-02-24 22:29:47 +0800

    Add [Easy] 7. Reverse Integer.

---
+++ solutions/typescript/problems/[Easy] 7. Reverse Integer.ts
@@ -0,0 +1,1 @@
+function reverse(x: number): number {    const MAX_INTEGER = Math.pow(2, 31) - 1;    const MIN_INTEGER = -Math.pow(2, 31);    const result = parseInt(x.toString().split("").reverse().join("")) * (x < 0 ? -1 : 1) || 0;    return result <= MAX_INTEGER && result >= MIN_INTEGER ? result : 0;}

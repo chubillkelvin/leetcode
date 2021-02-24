732615198fac43f08d5fcb7b96ac05f0d1f45dfc
RageBill <chubillkelvin@gmail.com>
2021-02-24 23:19:57 +0800

    Update [Easy] 9. Palindrome Number to solve problem without using string methods or allowing the number to overflow as 32-bit integer.

--- solutions/typescript/problems/[Easy] 9. Palindrome Number.ts
+++ solutions/typescript/problems/[Easy] 9. Palindrome Number.ts
@@ -1,3 +1,12 @@
 function isPalindrome(x: number): boolean {
-    return x >= 0 && x.toString() === x.toString().split("").reverse().join("");
+    // Solve without using conversion to string or overflowing the number
+    if (x < 0) return false; // negative numbers cannot be palindrome
+    if (x < 10) return true; // single digit numbers must be palindrome, including 0
+    if (x % 10 === 0) return false; // numbers ending with 0 (except 0 itself) cannot be palindrome
+    const maxExponent = Math.floor(Math.log10(x)); // until here, x must be > 10 and x cannot be a multiple of 10
+    let number = x;
+    for (let i = 0; i <= maxExponent; i++) {
+        number -= Math.pow(10, i) * Math.floor((x / Math.pow(10, maxExponent - i)) % 10);
+    }
+    return number === 0;
 }

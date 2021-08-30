4ab986681a618c5de7a4e13bbb99b64bf19d2dc0
Kelvin Chu <chubillkelvin@gmail.com>
2021-08-30 21:37:31 +0800

    Add remove-by-value.

---
+++ solutions/typescript/explore/linked-list/remove-by-value.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const tempHead = new ListNode(0, head);
+    let curr: ListNode | null = tempHead;
+    while (curr.next) {
+        if (curr.next.val === val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+
+    return tempHead.next;
+}

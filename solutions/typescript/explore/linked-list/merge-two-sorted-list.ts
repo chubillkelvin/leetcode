ee809e3fe2c19c4dde6fdd5822a4806e5948277d
Kelvin Chu <chubillkelvin@gmail.com>
2021-08-31 15:28:37 +0800

    Add merge-two-sorted-list.

---
+++ solutions/typescript/explore/linked-list/merge-two-sorted-list.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const pointerToHead = new ListNode();
+    let curr = pointerToHead;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
+        } else {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+    if (l1 === null) {
+        curr.next = l2;
+    }
+    if (l2 === null) {
+        curr.next = l1;
+    }
+
+    return pointerToHead.next;
+}

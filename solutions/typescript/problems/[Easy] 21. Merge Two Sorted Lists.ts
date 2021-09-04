7ee34a397d613820c5824741d1cde7ff014904b2
Kelvin Chu <chubillkelvin@gmail.com>
2021-09-04 22:19:56 +0800

    Move all explore answers into corresponding problems.

--- solutions/typescript/problems/[Easy] 21. Merge Two Sorted Lists.ts
+++ solutions/typescript/problems/[Easy] 21. Merge Two Sorted Lists.ts
@@ -1,38 +1,35 @@
-// Definition for singly-linked list.
-// @ts-ignore
-class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = val === undefined ? 0 : val;
-        this.next = next === undefined ? null : next;
-    }
-}
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 // @ts-ignore
 function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
     const pointerToHead = new ListNode();
-    let currentNode = pointerToHead;
-    while (l1 || l2) {
-        if (l1 && l2) {
-            if (l1.val <= l2.val) {
-                currentNode.next = l1;
-                currentNode = currentNode.next;
-                l1 = l1.next;
-            } else {
-                currentNode.next = l2;
-                currentNode = currentNode.next;
-                l2 = l2.next;
-            }
-        } else if (l1) {
-            currentNode.next = l1;
-            currentNode = currentNode.next;
+    let curr = pointerToHead;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            curr.next = l1;
             l1 = l1.next;
-        } else if (l2) {
-            currentNode.next = l2;
-            currentNode = currentNode.next;
+        } else {
+            curr.next = l2;
             l2 = l2.next;
         }
+        curr = curr.next;
     }
+    if (l1 === null) {
+        curr.next = l2;
+    }
+    if (l2 === null) {
+        curr.next = l1;
+    }
+
     return pointerToHead.next;
 }
--- solutions/typescript/problems/[Medium] 2. Add Two Numbers.ts
+++ solutions/typescript/problems/[Medium] 2. Add Two Numbers.ts
@@ -1,28 +1,30 @@
-// Definition for singly-linked list.
-// @ts-ignore
-class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = val === undefined ? 0 : val;
-        this.next = next === undefined ? null : next;
-    }
-}
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    const head = new ListNode();
-    let currentNode: ListNode = head;
-    while (l1 || l2) {
-        const sum: number = (l1?.val ?? 0) + (l2?.val ?? 0) + currentNode.val;
-        currentNode.val = sum % 10;
-        currentNode.next = new ListNode(sum >= 10 ? 1 : 0);
+    const headPointer = new ListNode(0);
+    let carryOver = 0;
+    let curr: ListNode = headPointer;
+    while (l1 || l2 || carryOver) {
+        curr.next = new ListNode((l1?.val || 0) + (l2?.val || 0) + carryOver);
+        curr = curr.next;
         l1 = l1?.next || null;
         l2 = l2?.next || null;
-        if (l1 || l2) {
-            currentNode = currentNode.next;
-        } else if (currentNode.next.val === 0) {
-            currentNode.next = null;
+        if (curr.val >= 10) {
+            curr.val -= 10;
+            carryOver = 1;
+        } else {
+            carryOver = 0;
         }
     }
-    return head;
+    return headPointer.next;
 }

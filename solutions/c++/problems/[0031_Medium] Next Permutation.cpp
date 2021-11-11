010c4967826ff4ae1dd14854e1755a2031d258e1
Kelvin Chu <chubillkelvin@gmail.com>
2021-11-12 01:08:35 +0800

    Add Next Permutation.

---
+++ solutions/c++/problems/[0031_Medium] Next Permutation.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int> &nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};

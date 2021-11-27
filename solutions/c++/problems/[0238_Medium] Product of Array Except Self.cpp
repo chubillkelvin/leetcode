f54ed59fae165c775f6a1f7b792265bcd1072036
Kelvin Chu <chubillkelvin@gmail.com>
2021-11-27 12:15:43 +0800

    Update Product of Array Except Self.

--- solutions/c++/problems/[0238_Medium] Product of Array Except Self.cpp
+++ solutions/c++/problems/[0238_Medium] Product of Array Except Self.cpp
@@ -8,17 +8,15 @@ public:
         int zeroCounts = count(nums.begin(), nums.end(), 0);
         if (zeroCounts >= 2) return vector<int>(nums.size(), 0);
 
-        vector<int> prodLeft = vector<int>(nums.size(), 1);
-        for (int i = 1; i < nums.size(); i++) {
-            prodLeft[i] = nums[i - 1] * prodLeft[i - 1];
-        }
         vector<int> prodRight = vector<int>(nums.size(), 1);
         for (int i = nums.size() - 2; i >= 0; i--) {
             prodRight[i] = nums[i + 1] * prodRight[i + 1];
         }
+        int prodLeft = 1;
         vector<int> ans = vector<int>(nums.size(), 1);
         for (int i = 0; i < nums.size(); i++) {
-            ans[i] = prodLeft[i] * prodRight[i];
+            if (i > 0) prodLeft *= nums[i - 1];
+            ans[i] = prodLeft * prodRight[i];
         }
         return ans;
     }

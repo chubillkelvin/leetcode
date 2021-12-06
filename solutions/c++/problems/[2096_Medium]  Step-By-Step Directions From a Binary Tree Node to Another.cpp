e488369d53131bf05c851d162f955ed7ccf45af4
Kelvin Chu <chubillkelvin@gmail.com>
2021-12-06 23:55:21 +0800

    Add Step-By-Step Directions From a Binary Tree Node to Another.

---
+++ solutions/c++/problems/[2096_Medium]  Step-By-Step Directions From a Binary Tree Node to Another.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool dfs(TreeNode *curr, int target, vector<char> *res) {
+        if (curr->val == target) return true;
+        if (curr->left && dfs(curr->left, target, res)) {
+            res->push_back('L');
+            return true;
+        }
+        if (curr->right && dfs(curr->right, target, res)) {
+            res->push_back('R');
+            return true;
+        }
+        return false;
+    }
+
+    string getDirections(TreeNode *root, int startValue, int destValue) {
+        vector<char> toStart = {};
+        vector<char> toEnd = {};
+
+        dfs(root, startValue, &toStart);
+        dfs(root, destValue, &toEnd);
+        while (!toStart.empty() && !toEnd.empty() && toStart.back() == toEnd.back()) {
+            toStart.pop_back();
+            toEnd.pop_back();
+        }
+        vector<char> fromStart = vector<char>(toStart.size(), 'U');
+        for (auto it = toEnd.rbegin(); it != toEnd.rend(); it++) fromStart.push_back(*it);
+        string ans = "";
+        for (char c: fromStart) ans += c;
+        return ans;
+    }
+};

14a309882a9f389c4eac8162786ba65b64cce6c4
Kelvin Chu <chubillkelvin@gmail.com>
2021-12-22 21:47:01 +0800

    Add First Bad Version.

---
+++ solutions/c++/problems/[0278_Easy] First Bad Version.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// The API isBadVersion is defined for you.
+//bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+};

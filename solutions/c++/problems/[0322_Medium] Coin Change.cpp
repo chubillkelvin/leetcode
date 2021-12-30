5d436932a2f6084cfc3475a17ec04e285ff40fdb
Kelvin Chu <chubillkelvin@gmail.com>
2021-12-31 01:16:43 +0800

    Update Coin Change.

--- solutions/c++/problems/[0322_Medium] Coin Change.cpp
+++ solutions/c++/problems/[0322_Medium] Coin Change.cpp
@@ -1,32 +1,24 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution {
 public:
     int coinChange(vector<int> &coins, int amount) {
-        int n = coins.size();
-        map<int, int> ans;
-        ans[0] = 0;
-        for (auto coin: coins) {
-            ans[coin] = 1;
+        if (amount == 0) return 0;
+        sort(coins.begin(), coins.end());
+        vector<int> counts = vector<int>(amount + 1, 0);
+        for (int coin: coins) {
+            if (coin <= amount) counts[coin] = 1;
         }
-        int maxCoins = amount / coins.back() + 1;
-        for (int i = 0; i <= amount; i++) {
-            if (ans.find(i) == ans.end()) continue;
-            int currCount = ans[i];
-            for (auto coin: coins) {
-                long next = (long) coin + i;
-                if (next <= amount) {
-                    auto nextVal = ans.find(next);
-                    if (nextVal == ans.end()) {
-                        ans[next] = currCount + 1;
+        for (int i = 0; i <= amount - coins[0]; i++) {
+            if (counts[i] == 0) continue;
+            for (int coin: coins) {
+                if ((long) i + coin <= amount) {
+                    if (counts[i + coin] == 0) {
+                        counts[i + coin] = counts[i] + 1;
                     } else {
-                        ans[next] = min(ans[next], currCount + 1);
+                        counts[i + coin] = min(counts[i + coin], counts[i] + 1);
                     }
                 }
             }
         }
-        return ans.find(amount) == ans.end() ? -1 : ans[amount];
+        return counts[amount] == 0 ? -1 : counts[amount];
     }
 };

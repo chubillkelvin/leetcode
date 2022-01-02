5203cdc14c51a398f944b33734905a58293972db
Kelvin Chu <chubillkelvin@gmail.com>
2022-01-02 12:08:37 +0800

    Update Pairs of Songs With Total Durations Divisible by 60.

--- solutions/c++/problems/[1010_Medium] Pairs of Songs With Total Durations Divisible by 60.cpp
+++ solutions/c++/problems/[1010_Medium] Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int> &time) {
         int n = time.size();
-        multiset<int> timeSet;
+        vector<int> timeSet(60);
         int count = 0;
         for (int i = 0; i < n; i++) {
             int curr = time[i];
-            count += timeSet.count((60 - curr % 60) % 60);
-            timeSet.insert(curr % 60);
+            count += timeSet[(60 - curr % 60) % 60];
+            timeSet[curr % 60]++;
         }
         return count;
     }

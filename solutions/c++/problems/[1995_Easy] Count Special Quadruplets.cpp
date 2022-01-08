a7fd7891046a17d2ad26bc715c5c5009b5f1e750
Kelvin Chu <chubillkelvin@gmail.com>
2022-01-08 19:40:37 +0800

    Update Count Special Quadruplets.

--- solutions/c++/problems/[1995_Easy] Count Special Quadruplets.cpp
+++ solutions/c++/problems/[1995_Easy] Count Special Quadruplets.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int countQuadruplets(vector<int> &nums) {
         int n = nums.size();
+        unordered_map<int, int> memo;
+        memo[nums[n - 1] - nums[n - 2]]++;
         int count = 0;
-        for (int i = 3; i < n; i++) {
-            int target = nums[i];
-            for (int x = 0; x < i; x++) {
-                for (int y = x + 1; y < i; y++) {
-                    for (int z = y + 1; z < i; z++) {
-                        int sum = nums[x] + nums[y] + nums[z];
-                        if (sum == target) count++;
-                    }
-                }
+        for (int b = n - 3; b >= 1; b--) {
+            for (int a = 0; a < b; a++) {
+                count += memo[nums[a] + nums[b]];
+            }
+
+            for (int d = n - 1; d >= b + 1; d--) {
+                memo[nums[d] - nums[b]]++;
             }
         }
         return count;

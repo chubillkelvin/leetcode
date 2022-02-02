8389274f362c6caf33faa5deebbd0a6c400e6d35
Kelvin Chu <chubillkelvin@gmail.com>
2022-02-02 22:09:22 +0800

    Add Implement Magic Dictionary.

---
+++ solutions/c++/problems/[0676_Medium] Implement Magic Dictionary.cpp
@@ -0,0 +1,34 @@
+class MagicDictionary {
+    unordered_map<int, vector<string>> dict;
+public:
+    MagicDictionary() {
+
+    }
+
+    void buildDict(vector <string> dictionary) {
+        for (auto s: dictionary) {
+            dict[s.size()].push_back(s);
+        }
+    }
+
+    bool search(string searchWord) {
+        int n = searchWord.size();
+        auto dictSet = dict[n];
+        if (dictSet.size() == 0) return false;
+        for (auto word: dictSet) {
+            int diff = 0;
+            for (int i = 0; i < n; i++) {
+                if (word[i] != searchWord[i]) diff++;
+            }
+            if (diff == 1) return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */

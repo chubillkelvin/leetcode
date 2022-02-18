10906573915dfb5a79c99f6c13ca10be7a348681
Kelvin Chu <chubillkelvin@gmail.com>
2022-02-18 23:47:19 +0800

    Update Network Delay Time.

--- solutions/c++/problems/[0743_Medium] Network Delay Time.cpp
+++ solutions/c++/problems/[0743_Medium] Network Delay Time.cpp
@@ -1,12 +1,12 @@
 class Solution {
     int n;
 public:
-    int findMin(vector<int> &dist, unordered_set<int> &mst) {
-        int minVal = INT_MAX;
+    int findMin(vector<int> &dist, vector<bool> &spt) {
+        int minDist = INT_MAX;
         int minIndex = -1;
         for (int i = 0; i < n; i++) {
-            if (mst.count(i) == 0 && dist[i] < minVal) {
-                minVal = dist[i];
+            if (!spt[i] && dist[i] < minDist) {
+                minDist = dist[i];
                 minIndex = i;
             }
         }
@@ -15,26 +15,24 @@ public:
 
     int networkDelayTime(vector <vector<int>> &times, int n, int k) {
         this->n = n;
+        vector <vector<int>> adj(n, vector<int>(n, -1));
+        for (auto time: times) adj[time[0] - 1][time[1] - 1] = time[2];
         vector<int> dist(n, INT_MAX);
-        unordered_set<int> mst;
+        vector<bool> spt(n, false);
         dist[k - 1] = 0;
-        int maxTime = 0;
-        for (int i = 0; i < n; i++) {
-            int index = findMin(dist, mst);
-            if (index == -1) return -1;
-            mst.insert(index);
-            if (i < n - 1) {
-                for (int j = 0; j < times.size(); j++) {
-                    int from = times[j][0] - 1;
-                    int to = times[j][1] - 1;
-                    int distance = times[j][2];
-                    if (mst.count(from) > 0 && mst.count(to) == 0) {
-                        dist[to] = min(dist[to], dist[from] + distance);
-                    }
-                }
-            } else {
-                maxTime = dist[index];
+        for (int i = 0; i < n - 1; i++) {
+            int idx = findMin(dist, spt);
+            if (idx == -1) return -1;
+            spt[idx] = true;
+            for (int j = 0; j < n; j++) {
+                if (spt[j] || adj[idx][j] == -1) continue;
+                dist[j] = min(dist[j], dist[idx] + adj[idx][j]);
             }
+        }
+        int maxTime = 0;
+        for (auto d: dist) {
+            if (d == INT_MAX) return -1;
+            maxTime = max(maxTime, d);
         }
         return maxTime;
     }

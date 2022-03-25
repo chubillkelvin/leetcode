051d1e4905e5697999ae8b4d491727d644d870e0
Kelvin Chu <chubillkelvin@gmail.com>
2022-03-25 20:36:25 +0800

    Add Broken Calculator.

---
+++ solutions/java/problems/[0991_Medium] Broken Calculator.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int count = 0;
+        while (target != startValue) {
+            if (target < startValue || target % 2 != 0) {
+                target++;
+            } else {
+                target /= 2;
+            }
+            count++;
+        }
+        return count;
+    }
+}
